6d085060e543a2c283d074d1817e50795e20c123
Julianar99 <140024925+Julianar99@users.noreply.github.com>
2023-09-03 02:18:01 -0300

    uni archivo java

--- js/java.js
+++ js/java.js
@@ -1,11 +1,4 @@
-let nombre ="juliana";
-let apellido ="rozich";
-console.log(nombre+"  "+apellido);
-
-let numero1= 100;
-let numero2=150;
-let resultadosuma= numero1+numero2;
-console.log("suma:  ",resultadosuma);
-let numero=60;
-let numero3=(resultadosuma+numero);
-console.log(numero3);
+function saludo(persona){
+    console.log("hola"+ persona);
+}
+saludo("kiara");
