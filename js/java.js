7df5aab5c9f1b184e8731689758a8edc38a4aa07
Julianar99 <140024925+Julianar99@users.noreply.github.com>
2023-09-03 22:22:07 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,4 +1,37 @@
-function saludo(persona){
-    console.log("hola"+ persona);
+let helado = 30;
+let topping = ['oreo','kitkat','kinder','nada'];
+let precio;
+let preciofinal;
+let top1=10;
+let top2=15;
+let top3=25;
+let top4=0
+if (helado=helado + topping(oreo)) { let resultadosuma1= helado + top1
+    console.log("precio: ",resultadosuma1 );
+} else if (helado=topping(kitkat)){ let resultadosuma2= helado 
+    top2
+    console.log("precio: ",resultadosuma2 );
+} else if (helado=topping(kinder)){ let resultadosuma3= helado + top3
+    console.log("precio: ",resultadosuma3);
+} else (helado=topping(ninguno))=>{let resultadosma4= helado + top4
+    console.log("precio: ",resultadosma4 );
 }
-saludo("kiara");
+
+
+let comida="menu";
+switch (comida) {
+    case "carne":
+    console.log("se le servira con vino tinto");
+    break;
+    case "percado":
+        console.log("se le servira con vino blanco");
+        break;
+        case "verdura":
+            console.log("se le servira con agua");
+            break;
+            default:
+                console.log("elija en el menu carne , percado o verdura");
+                for ( let i=0 ; i=4){
+                    console.log(menu[i])
+                }
+}
