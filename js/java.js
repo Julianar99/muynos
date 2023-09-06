c369e7ba86cac1616488acaef820a8301ec67312
Julianar99 <140024925+Julianar99@users.noreply.github.com>
2023-09-05 22:49:54 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,3 +1,13 @@
 var nom;
-nom=prompt("escribe tu nombre","Nombre");
-alert("mucho gusto" +  + nom);
+nom=prompt("Escribe tu nombre","Nombre");
+alert("Mucho gusto" +  nom);
+
+
+window:alert("Bienvenida a nuestra web");
+
+alert =false;
+const interval = setInterval(()=>{
+
+    document.title=alert?'Muy Nosotras':'(1) New follower';
+    alert=!alert;
+},1000)
